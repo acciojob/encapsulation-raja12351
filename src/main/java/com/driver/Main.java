3413590ce3d9274e67e619a89b953a5cf9fa0762
raja12351 <129305916+raja12351@users.noreply.github.com>
2023-05-11 15:58:34 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  RWOnly obj = new RWOnly();
+// obj.name = "rajat";
+//java: name has private access in RWOnly --> Error shown by the Compiler
+  obj.setName("Rajat");
+  System.out.println(obj.getName());
 }
